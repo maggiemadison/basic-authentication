96c4689a4e0395562754e6e1a15e5dfa5092ddc6
Melissa McGuire <74629223+maggiemadison@users.noreply.github.com>
2021-12-11 17:12:05 -0600

    Creating the protectedRoute Hook

--- src/Protected.js
+++ src/Protected.js
@@ -1,19 +1,23 @@
 import React, { useEffect } from 'react';
 import { Auth } from 'aws-amplify'
 import Container from './Container'
+import { useNavigate } from 'react-router-dom'
 
-function Protected(props) {
-  useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .catch(() => {
-        props.history.push('/profile')
-      })
-  }, [])
   return (
-    <Container>
-      <h1>Protected route</h1>
-    </Container>
-  );
-}
 
-export default Protected
+const Protected = (props) => {
+    const navigate = useNavigate();
+    useEffect(() => {
+      Auth.currentAuthenticatedUser()
+        .catch(() => {
+          navigate('/profile')
+        })
+    }, [])
+    return (
+      <Container>
+        <h1>Protected route</h1>
+      </Container>
+    );
+  }
+  
+  export default Protected
--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { HashRouter, Switch, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 import Nav from './Nav'
 import Public from './Public'
@@ -21,12 +21,12 @@ const Router = () => {
   return (
     <HashRouter>
       <Nav current={current} />
-      <Switch>
-        <Route exact path="/" component={Public}/>
-        <Route exact path="/protected" component={Protected} />
-        <Route exact path="/profile" component={Profile}/>
-        <Route component={Public}/>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Public />}/>
+        <Route exact path="/protected" element={<Protected />} />
+        <Route exact path="/profile" element={<Profile />}/>
+        <Route element={<Public />}/>
+      </Routes>
     </HashRouter>
   )
 }
